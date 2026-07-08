0921119d8cb45262158978964739cab0a58eaf99
nidhy <nidhy@Nidhya>
2026-07-08 15:23:41 -0500

    8July2026-Trial

--- SampleMaven/src/main/java/inheritance/Access3.java
+++ SampleMaven/src/main/java/inheritance/Access3.java
@@ -6,7 +6,7 @@ public class Access3 extends Access1{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Access1 obj=new Access1(); //Access1 has to be imported to Access 3
+		Access1 obj=new Access1(); //Access1 has to be imported to Access 3 --
 		//obj.display1();
 		//obj.display3();
 		Access3 obj2=new Access3();
